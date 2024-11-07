fa70931fe01a9ca93a918eb1fa72f45e9159ed6a
dyfinsta <alvorddylan9@gmail.com>
2024-11-07 17:49:45 -0500

    Update Review.java
    Initialized Review.java with default constructor and getter/setter methods for attributes

---
+++ RestaurantApp/src/main/java/com/app/review/Review.java
@@ -0,0 +1,32 @@
+package com.app.review;
+
+import java.util.ArrayList;
+
+public class Review {
+
+    private boolean isPositive;
+    private ArrayList<String> contents;
+
+    public Review(boolean isPositive, ArrayList<String> contents){
+        this.isPositive = isPositive;
+        this.contents = contents;
+    }
+
+    public boolean getIsPositive(){
+        return isPositive;
+    }
+
+    public ArrayList<String> getContents(){
+        return contents;
+    }
+
+    public void setIsPositive(boolean isPositive){
+        this.isPositive = isPositive;
+    }
+
+    public void setContents(ArrayList<String> contents){
+        this.contents = contents;
+    }
+
+
+}
