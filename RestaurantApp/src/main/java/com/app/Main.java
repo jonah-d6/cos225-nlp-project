c415c8a7f8523a8e1b6b78b38566489ecfa24854
jonah-d6 <jonah.dean@maine.edu>
2024-11-14 18:02:30 -0500

    Add barebones for parsing the csv files

--- RestaurantApp/src/main/java/com/app/Main.java
+++ RestaurantApp/src/main/java/com/app/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+
 import com.app.Review;
 
 public class Main
@@ -8,6 +10,17 @@ public class Main
     String fileName = "../../../resources/restaurant_reviews.csv"
     try(BufferedReader reader = new BufferedReader(new FileReader(fileName)))
     {
+      String line = reader.nextLine();
+      Review currentLine;
+      ArrayList<String> contents;
+      int reviewScore;
+      while (line != null)
+      {
+        //TODO: fill up contents with the words in the review
+        //TODO: initialize reviewScore with the final value, separated by the comma
+        currentLine = (reviewScore, contents);
+        line = reader.nextLine();
+      }
     }
     catch(Exception e)
     {
