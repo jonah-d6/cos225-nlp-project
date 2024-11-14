7d19a2b59e7286f77bd3c3d694b784e3759183e1
jonah-d6 <jonah.dean@maine.edu>
2024-11-14 17:57:17 -0500

    Add start of parsing in startUp method

--- RestaurantApp/src/main/java/com/app/Main.java
+++ RestaurantApp/src/main/java/com/app/Main.java
@@ -1,4 +1,18 @@
+import java.io.*;
+import com.app.Review;
+
 public class Main
 {
-
+  private static void startUp()
+  {
+    String fileName = "../../../resources/restaurant_reviews.csv"
+    try(BufferedReader reader = new BufferedReader(new FileReader(fileName)))
+    {
+    }
+    catch(Exception e)
+    {
+      System.out.println("Something happened while trying to read the file");
+      e.printStackTrace();
+    }
+  }
 }
