9b4e917f4d84a7ba4f0fd78fc1925c5f3ebc2d00
jonah-d6 <jonah.dean@maine.edu>
2024-11-21 11:56:05 -0500

    fix up pseudo-code to be a bit easier to implement

--- RestaurantApp/src/main/java/com/app/classifier/Classifier.java
+++ RestaurantApp/src/main/java/com/app/classifier/Classifier.java
@@ -2,16 +2,20 @@ package com.app.classifier
 
 public class Classifier
 {
-  P(+ve) = numPosDocs / numTotalDocs;
-  P(-ve) = numNegDocs / numTotalDocs;
-
-  for (String word : review)
+  public boolean classify(String[] words)
   {
-    P(word|+ve) = wordCountInPosContext / allCountInNegContext;
-    P(word|-ve) = wordCountInNegContext / allCountInNegContext;
-  }
+    P(+ve) = numPosDocs / numTotalDocs;
+    P(-ve) = numNegDocs / numTotalDocs;
 
-  P(R|+ve) = P(+ve) * {PRODUCT(P(word|+ve))}
-  P(R|-ve) = P(-ve) * {PRODUCT(P(word|-ve))}
-  
+    P(R|+ve) = P(+ve);
+
+    for (String word : words)
+    { //adding vocabulary to denominator is optional here
+      P(word|+ve) = (wordCountInPosContext+1) / (allCountInNegContext+vocabulary);
+      P(word|-ve) = (wordCountInNegContext+1) / (allCountInNegContext+vocabulary);
+
+      P(R|+ve) *= P(word|+ve);
+      P(R|-ve) *= P(word|-ve);
+    }
+  }
 }
