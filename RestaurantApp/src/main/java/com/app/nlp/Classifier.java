5f2c1ea6ff2fba23e298524c1f68981fd97a24bf
jonah-d6 <jonah.dean@maine.edu>
2024-12-05 11:50:55 -0500

    Add basic items to Classifier.java
    Added imports, private attributes, empty constructor, and an updateTraining method.

--- RestaurantApp/src/main/java/com/app/nlp/Classifier.java
+++ RestaurantApp/src/main/java/com/app/nlp/Classifier.java
@@ -1,2 +1,34 @@
 package com.app.nlp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Classifier
+{
+  private HashSet<String> vocabulary = new HashSet<>();
+
+  private int numPositiveReviews = 0;
+  private int numNegativeReviews = 0;
+  private int numTotalReviews = 0;
+
+  private HashMap<String, Integer> positiveWordCount = new HashMap<>();
+  private HashMap<String, Integer> negativeWordCount = new HashMap<>();
+
+  private HashMap<String, Double> positiveProbabilites = new HashMap<>();
+  private HashMap<String, Double> negativeProbabilites = new HashMap<>();
+
+  public Classifier()
+  {
+  }
+
+  public void updateTraining(Review trainer)
+  {
+    ArrayList<String> words = trainer.trimContents();
+    if (review.getIsPositive())
+      updateWordCount(positiveWordCount, words);
+    else
+      updateWordCount(negativeWordCount, words);
+  }
+  
+}
